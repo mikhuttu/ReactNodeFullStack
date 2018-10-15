106b9b1d40da2c19fc72fb97d9cc07785141f8aa
Mika Huttunen <mika.huttunen@Mika-Huttunen.local>
2018-10-15 15:04:28 +0300

    changed part 1 so that it doesn't contain RN files anymore. renamed other files

--- test/src/index.js
+++ test/src/index.js
@@ -1,42 +1,8 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-const Display = ({counter}) => <div>Counter value: {counter}</div>
-
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 )
-
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      counter: 0
-    }
-  }
-
-  setValue = (value) => () => this.setState({ counter: value})
-
-  render() {
-    return (
-      <div>
-        <Display counter={this.state.counter}/>
-        <div>
-          <Button
-            handleClick={this.setValue(this.state.counter + 1)}
-            text="Increment" />
-          <Button
-            handleClick={this.setValue(this.state.counter - 1)}
-            text="Decrement" />
-          <Button
-            handleClick={this.setValue(0)}
-            text="Reset" />
-        </div>
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
